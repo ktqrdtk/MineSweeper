b67753da6c642efeefec4d2e7400f58115cd7963
KTQRDTK <KTQRDTK@CCSD-45JYBH2.ccsdut.net>
2018-04-26 14:42:36 -0600

    Start of classes

--- MineSweeperInitial/src/main/Game.java
+++ MineSweeperInitial/src/main/Game.java
@@ -4,6 +4,6 @@ public class Game
 {
 	public static void main(String[] args)
 	{
-		System.out.print(true);
+		
 	}
 }
---
+++ MineSweeperInitial/src/main/Location.java
@@ -0,0 +1,12 @@
+package main;
+import javax.swing.JButton;
+
+public class Location extends JButton
+{
+	private static final long serialVersionUID = 1L;
+	
+	public Location()
+	{
+		
+	}
+}
